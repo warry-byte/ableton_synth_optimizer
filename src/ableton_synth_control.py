46dfcafe1e5ef7bd70e41a41281f728fe9a532cc
warry-byte <antoun.aj@gmail.com>
2023-12-16 14:58:37 +0100

    Added user evaluation dialog
    Added user evaluation dialog (debugging) and simple Ableton project with one instrument and simple clip, so the user can listen to what they are optimizing.

--- src/ableton_synth_control.py
+++ src/ableton_synth_control.py
@@ -9,10 +9,60 @@ Created on Thu Dec 14 18:22:30 2023
 #%% Imports
 import live
 import random
+from skopt import BayesSearchCV
+import tkinter as tk
+
+#%% User defined functions
+# Define objective function with user evaluations
+def objective_function():
+    global set
+    global user_evaluation
+    global root
+    
+    set.start_playing()
+    # Assuming your user provides evaluations (0 or 1 for bad/good) interactively
+    root = tk.Tk()
+    root.title("Rate what you are hearing")
+    
+    # Button for "Good"
+    good_button = tk.Button(root, text="Good", command=lambda: set_evaluation(1))
+    good_button.pack(side=tk.LEFT, padx=10)
+
+    # Button for "Bad"
+    bad_button = tk.Button(root, text="Bad", command=lambda: set_evaluation(0))
+    bad_button.pack(side=tk.RIGHT, padx=10)
+    
+    # Button for "Close"
+    bad_button = tk.Button(root, text="Close", command=lambda: close)
+    bad_button.pack(side=tk.RIGHT, padx=10)
+    
+    root.mainloop()
+    
+    return -user_evaluation  # Minimize, so use negative of user evaluation
+
+# Return different evaluation and kill open dialog
+def set_evaluation(value):
+    global user_evaluation
+    user_evaluation = value
+    
+    if user_evaluation == 0:
+        print("User evaluation: BAD!")
+    else: 
+        print("User evaluation: GOOD!")
+    
+    
+def close():
+    global root
+    global set
+    set.stop_playing()
+    root.destroy()
+    exit()
 
 #%% Synth information
 synth_name = "DS Clang"
 excluded_param = ["Volume", "Device On"] # List of parameters that will not be part of the optimization process
+user_evaluation = 0
+root = None
 
 #%% Get Live object
 set = live.Set()
@@ -30,6 +80,7 @@ for p in param_list:
     
 relevant_parameters_list = [ele for ele in param_list if (ele.name not in excluded_param)]
 
-#%% Init optimizer
-a = 0 
+#%% Test objective function
+objective_function()
+
     

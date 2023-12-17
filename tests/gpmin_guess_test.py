0bc3898b646510e6c5c19ff49e7bb8afd9cddadc
warry-byte <antoun.aj@gmail.com>
2023-12-17 19:39:23 +0100

    ADD: Example of guess game using gp_minimize. User evaluation of the pair uses a number which the user sets arbitrarily close to 0 as the script approaches the pair. Convergence has increased. User input is noisy as hell. I don't know how I could express my evaluation function in terms of these two numbers. But who cares, it seems to converge... Anyway it is not as if the quality of a synth preset was objective after all... I should maybe change number pairs while evaluating it...

---
+++ tests/gpmin_guess_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 12:00:04 2023
+
+@author: warrybyte
+"""
+
+from skopt import Optimizer, Space, gp_minimize
+from skopt.space import Integer, Real
+from skopt.plots import plot_convergence
+
+# Function to be optimized (returning a score based on user feedback)
+def evaluate_numbers(guess):
+    print(f"My guess: {guess}")
+    feedback = input("How close is it to your pair? ")
+
+    return int(feedback)
+
+
+print("I will try to guess the two numbers you have in mind...")
+print("Please help me in my search!")
+
+search_space = Space([Integer(low=0, high=10, prior='uniform', transform='normalize'),
+                      Integer(low=0, high=10, prior='uniform', transform='normalize')])
+
+
+# Optimize the numbers
+res = gp_minimize(func=evaluate_numbers,
+                  dimensions=search_space,
+                  n_calls=20,
+                  n_initial_points=5,
+                  acq_func='EI',
+                  acq_optimizer='sampling',
+                  noise="gaussian")
+
+# Get the best numbers
+best_numbers = res.x
+print("Best Numbers:", best_numbers)
+plot_convergence(res)
+

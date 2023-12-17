50babaf9c0c4add403e63c34bb2fad34f7d16233
warry-byte <antoun.aj@gmail.com>
2023-12-17 19:09:22 +0100

    ADD: Supervised learning example for tuning convergence
    Added test script for tuning convergence based on user evaluation. The script tries to guess two numbers and the user tells 1 if it gets closer to the pair, -1 if not (bad solution), or 0 if it is correct.
    Obviously, it should stop if 0, but more importantly, the optimizer gets stuck in the boundaries of the search space. Making params Real solves this issue, but decreases convergenve speed.

---
+++ tests/supervised_learning_example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 12:00:04 2023
+
+@author: warrybyte
+"""
+
+from skopt import Optimizer, Space
+from skopt.space import Integer
+from skopt.plots import plot_convergence
+
+# Function to be optimized (returning a score based on user feedback)
+def evaluate_numbers(guess):
+    print(f"My guess: {guess}")
+    feedback = input("Is it good, bad, or correct? ").lower()
+
+    if feedback == "correct":
+        return 0  # Score 0 for correct guess
+    elif feedback == "good":
+        return 1  # Score 1 for a good guess
+    else:
+        return -1  # Score -1 for a bad guess
+
+# create a standard scaler transformer
+#transformer = Transformer()
+
+# set the transformer for each dimension in the search space
+#for dim in param_space:
+#    dim.set_transformer(transformer)
+
+
+search_space = Space([Integer(low=0, high=10, prior='uniform', transform='normalize'),
+                      Integer(low=0, high=10, prior='uniform', transform='normalize')])
+
+# Initialize the optimizer
+opt = Optimizer(search_space,
+                "GP", 
+                acq_func="gp_hedge",
+                acq_optimizer="sampling",
+                initial_point_generator="lhs")
+
+
+# Optimize the numbers
+res = None
+
+for _ in range(20):  # Adjust the number of iterations as needed
+    suggestion = opt.ask()
+    score = evaluate_numbers(suggestion)
+    res = opt.tell(suggestion, score)
+
+# Get the best numbers
+best_numbers = res.x
+print("Best Numbers:", best_numbers)
+plot_convergence(res)
+
